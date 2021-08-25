61e9ea3c15b6739bd1c2923e4f077bc07128c7b9
Michal Aron <michal.aron@monster.com>
2021-08-25 14:45:18 +0200

    Added defaultRegion parameter

--- src/main/java/com/example/libphonenumberdemo/controller/PhoneNumberCheckController.java
+++ src/main/java/com/example/libphonenumberdemo/controller/PhoneNumberCheckController.java
@@ -7,26 +7,31 @@ import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
 import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("check")
 public class PhoneNumberCheckController {
 
-  @ApiOperation("Phone number check")
+  @Operation(summary = "Phone number check")
   @GetMapping(
       value = "/number",
       produces = { "application/json" }
   )
-  public PhoneNumberCheckResultDTO check(PhoneNumberDTO phoneNumber) {
+  public PhoneNumberCheckResultDTO check(PhoneNumberDTO phoneNumber,
+                                         @ApiParam(value = "2 letter ISO 3166-1 alpha-2 country code, used only for numbers WITHOUT international prefix, for numbers with international prefix ignored. If not specified, 'US' would be used by default.",
+                                             defaultValue = "US") @RequestParam(required = false, defaultValue = "US") String defaultRegion) {
     String origNumberString = phoneNumber.getPhoneNumber();
     var result = new PhoneNumberCheckResultDTO();
 
     PhoneNumber libPhoneNumber;
     try {
-      libPhoneNumber = PhoneNumberUtil.getInstance().parse(origNumberString, "US");
+      libPhoneNumber = PhoneNumberUtil.getInstance().parse(origNumberString, defaultRegion);
     } catch (NumberParseException e) {
       result.setParsingError(e.getErrorType().toString() + " - " + e.getMessage());
       return  result;
